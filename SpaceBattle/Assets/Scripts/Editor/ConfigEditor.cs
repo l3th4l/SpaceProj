11d62791f42327436a48f570cae265b691b759d5
l3th4l <safalyaglobal@gmail.com>
2018-04-06 23:59:46 +0530

    made ship config files

---
+++ SpaceBattle/Assets/Scripts/Editor/ConfigEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class ConfigEditor : EditorWindow
+{
+    public PlayerConfig ConfigData;
+
+    private string ConfigProjectFilePath = "/StreamingAssets/Player.json";
+
+    private void LoadConfig()
+    {
+        string ConfigPath = Application.dataPath + ConfigProjectFilePath;
+
+        if(File.Exists(ConfigPath))
+        {
+            string ConfigDataAsJSON = File.ReadAllText(ConfigPath);
+
+            ConfigData = JsonUtility.FromJson<PlayerConfig>(ConfigDataAsJSON);
+        }
+        else
+        {
+            ConfigData = new PlayerConfig();
+        }
+    }
+
+    private void SaveData()
+    {
+        string ConfigDataAsJSON = JsonUtility.ToJson(ConfigData);
+        string ConfigPath = Application.dataPath + ConfigProjectFilePath;
+
+        File.WriteAllText(ConfigPath, ConfigDataAsJSON);
+    }
+}
